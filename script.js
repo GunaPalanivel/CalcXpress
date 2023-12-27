83fc53b4e5a35663adc6696bcc44de6b6f7e0d43
GunaPalanivel <gunapalanivel2003@gmail.com>
2023-12-27 21:46:42 +0530

    Add scientific calculator features

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let display = document.getElementById('display');
-let expression = '';
+let display = document.getElementById("display");
+let expression = "";
 
 function appendToDisplay(value) {
   expression += value;
@@ -10,19 +10,40 @@ function calculate() {
   try {
     let result = eval(expression);
     display.value = result;
-    expression = '';
+    expression = "";
   } catch (error) {
-    display.value = 'Error';
-    expression = '';
+    display.value = "Error";
+    expression = "";
   }
 }
 
 function clearDisplay() {
-  display.value = '';
-  expression = '';
+  display.value = "";
+  expression = "";
 }
 
 function deleteLastEntry() {
   expression = expression.slice(0, -1);
+  display.value = expression;
+}
+
+// Additional Functions for Scientific Calculator Features
+function calculateSquareRoot() {
+  expression = Math.sqrt(eval(expression)).toString();
+  display.value = expression;
+}
+
+function calculatePower() {
+  expression = Math.pow(eval(expression)).toString();
+  display.value = expression;
+}
+
+function calculateSine() {
+  expression = Math.sin(eval(expression)).toString();
+  display.value = expression;
+}
+
+function calculateCosine() {
+  expression = Math.cos(eval(expression)).toString();
   display.value = expression;
 }
